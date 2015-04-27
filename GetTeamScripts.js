5febb74d8ac5d872f9359c0a65e8b0b356379075
Tellow Krinkle <etang110@gmail.com>
2015-04-27 14:52:27 -0500

    Better sorting
    Click a column name to sort by it.

--- GetTeamScripts.js
+++ GetTeamScripts.js
@@ -2,6 +2,12 @@ var form;
 var xmlhttp;
 var tableBody;
 var response;
+var sorts = [];
+var sortNames = [];
+
+addSorter("teamNumber", true);
+addSorter("matchNumber", true);
+
 if (window.XMLHttpRequest) {
 	// code for IE7+, Firefox, Chrome, Opera, Safari
 	xmlhttp = new XMLHttpRequest();
@@ -179,37 +185,143 @@ function clearTable(teams) {
 }
 
 function appendTable(teams) {
-	teams.sort(function(a, b) {
-		if (a.matchType == b.matchType) {
-			if (a.elimNumber && a.elimNumber != b.elimNumber) {
-				return a.elimNumber - b.elimNumber;
-			}
-			else {
-				return a.matchNumber - b.matchNumber;
-			}
+	// Don't touch the original array
+	var myTeams = teams.slice();
+	myTeams.sort(sorter);
+	for (team in myTeams) {
+		tableBody.appendChild(makeRow(myTeams[team]));
+	}
+}
+
+function addSorter(column, cancelRedisplay) {
+	if (sortNames[sortNames.length - 1] == column) {
+		sorts[sorts.length - 1] = reverseSorter(sorts[sorts.length - 1]);
+	}
+	else {
+		var currentPos = sortNames.indexOf(column);
+		if (currentPos > -1) {
+			sortNames.splice(currentPos, 1);
+			sorts.splice(currentPos, 1);
+		}
+		sortNames.push(column);
+		if (column == "matchNumber") {
+			sorts.push(sortByMatch);
+		}
+		else if (column == "allianceToteSet" || column == "allianceToteStack") {
+			sorts.push(makeSorter("allianceToteSet", "stackSet", "allianceToteStack"));
+		}
+		else if (column == "coopToteSet" || column == "coopToteStack") {
+			sorts.push(makeSorter("coopToteSet", "stackSet", "coopToteStack"));
+		}
+		else if (column == "notes") {
+			sorts.push(makeSorter(column, "text"));
+		}
+		else if (column == "teamNumber") {
+			sorts.push(makeSorter(column, "lowNumber"));
 		}
 		else {
-			if (a.matchType == "Qualifications") {
-				return -1;
+			sorts.push(makeSorter(column, "highNumber"));
+		}
+	}
+	if (!cancelRedisplay) {
+		redisplay();
+	}
+}
+
+function makeSorter(column, type, column2) {
+	if (column == undefined) {
+		return function() {return 0;}
+	}
+	if (type == undefined) {
+		type = "highNumber";
+	}
+	if (type == "highNumber") {
+		return function(a, b) {
+			return b[column] - a[column];
+		}
+	}
+	else if (type == "lowNumber") {
+		return function(a, b) {
+			return a[column] - b[column];
+		}
+	}
+	else if (type == "text") {
+		return function(a, b) {
+			if (a[column].toLowerCase() > b[column].toLowerCase()) {
+				return 1;
 			}
-			else if (a.matchType == "Quarterfinals" && b.matchType != "Qualifications") {
-				return -1;
-			}
-			else if (a.matchType == "Semifinals" && b.matchType != "Qualifications" && b.matchType != "Quarterfinals") {
-				return -1;
-			}
-			else if (a.matchType == "Finals" && b.matchType != "Qualifications" && b.matchType != "Quarterfinals" && b.matchType != "Semifinals") {
-				return -1;
-			}
-			else if (a.matchType == "Practice" && b.matchType == "Other") {
+			else if (a[column].toLowerCase() < b[column].toLowerCase()) {
 				return -1;
 			}
 			else {
-				return 1;
+				return 0;
 			}
 		}
-	});
-	for (team in teams) {
-		tableBody.appendChild(makeRow(teams[team]));
+	}
+	else if (type == "stackSet") {
+		if (column2 == undefined) {
+			return function() {return 0;}
+		}
+		return function(a, b) {
+			if (a[column2] > 0 && b[column2] > 0) {
+				return 0;
+			}
+			else {
+				var stack = b[column2] - a[column2];
+				if (stack == 0) {
+					return b[column] - a[column];
+				}
+				else {
+					return stack;
+				}
+			}
+		}
+	}
+}
+
+function sorter(a, b) {
+	for (var i = sorts.length - 1; i >= 0; i--) {
+		var order = sorts[i](a, b);
+		if (order != 0) {
+			return order;
+		}
+	}
+	return 0;
+}
+
+function reverseSorter(func) {
+	return function(a, b) {
+		return func(b, a);
+	}
+}
+
+function sortByMatch(a, b) {
+	if (a.matchType == b.matchType) {
+		if (a.elimNumber && a.elimNumber != b.elimNumber) {
+			return a.elimNumber - b.elimNumber;
+		}
+		else {
+			return a.matchNumber - b.matchNumber;
+		}
+	}
+	else {
+		if (a.matchType == "Qualifications") {
+			return -1;
+		}
+		else if (a.matchType == "Quarterfinals" && b.matchType != "Qualifications") {
+			return -1;
+		}
+		else if (a.matchType == "Semifinals" && b.matchType != "Qualifications" && b.matchType != "Quarterfinals") {
+			return -1;
+		}
+		else if (a.matchType == "Finals" && b.matchType != "Qualifications" && b.matchType != "Quarterfinals" && b.matchType != "Semifinals") {
+			return -1;
+		}
+		else if (a.matchType == "Practice" && b.matchType == "Other") {
+			return -1;
+		}
+		else {
+			return 1;
+		}
 	}
 }
